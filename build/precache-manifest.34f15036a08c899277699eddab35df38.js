927a68bbe6dd6fc2a0d557baa53d03e2f4255d5f
franco-canedo <francanedo1@hotmail.com>
2021-01-07 22:13:00 -0600

    build

---
+++ build/precache-manifest.34f15036a08c899277699eddab35df38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2068f2f5e8609bb6ad77a8ac4b04152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1af254d84c817f11baff",
+    "url": "/static/css/main.e3ca8238.chunk.css"
+  },
+  {
+    "revision": "dd316898d557d0763729",
+    "url": "/static/js/2.62d4bb1a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.62d4bb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af254d84c817f11baff",
+    "url": "/static/js/main.adee5df7.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
